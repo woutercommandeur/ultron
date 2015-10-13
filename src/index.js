057fcf075abfd22ae7886ff12c05d2ace46a1b08
Wouter Commandeur <wouter@ctlaltdel.nl>
2015-10-13 10:16:09 +0200

    add game namespace to core

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ core.linkedlist = require('./linkedlist');
 core.procedural = require('./procedural');
 core.timer = require('./timer');
 core.input = require('./input');
+core.game = require('./game');
 
 module.exports = core;
 
