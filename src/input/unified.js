8213bde2f7be23e51a557d30543d4b5bbc90e2a7
Wouter Commandeur <wouter@ctlaltdel.nl>
2016-01-10 14:40:25 +0100

    blah

--- src/input/unified.js
+++ src/input/unified.js
@@ -278,7 +278,7 @@ function handleGamePadButtonEvent(val, vcode, inputs) {
     if (!arr) {
         return;
     }
-    console.log(vcode);
+    console.log(vcode, val);
 
     var pressed = val === 1.0;
     var isPerc = false;
